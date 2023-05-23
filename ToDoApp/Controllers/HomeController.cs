9850a068c2a0f008a800e81424451e0a32fbab6f
Rav <Rav@DESKTOP>
2023-05-23 20:54:09 +0200

    Dodaj pliki projektów.

---
+++ ToDoApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ToDoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPageContent()
+        {
+            return PartialView("Content/ContentPartialView");
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+            return View();
+        }
+    }
+}
