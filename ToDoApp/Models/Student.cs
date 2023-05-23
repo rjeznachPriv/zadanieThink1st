ac331fc7881e0913f303618f067de7efcdd33f10
unknown <rafal.jeznach@interia.pl>
2023-05-23 23:37:34 +0200

    zadanie10

---
+++ ToDoApp/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models {
+    public class Student : Person {
+
+        [StringLength(250, ErrorMessage = "Univeristy should be shorter or equal to 250 characters")]
+        public string University { get; set; }
+    }
+}
